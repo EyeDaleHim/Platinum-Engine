1b8ec5fa53f5b2a3319acc64cf406efe0ff4e043
EyeDaleHim <eyedalehim306yt@gmail.com>
2021-07-22 18:56:21 +0800

    diffic

--- source/PauseSubState.hx
+++ source/PauseSubState.hx
@@ -15,16 +15,21 @@ import flixel.util.FlxColor;
 
 class PauseSubState extends MusicBeatSubstate
 {
-	var grpMenuShit:FlxTypedGroup<Alphabet>;
-
-	var menuItems:Array<String> = ['Resume', 'Restart Song', 'Exit to menu'];
+	public var grpMenuShit:FlxTypedGroup<Alphabet>;
+	public var menuItems:Array<String> = ['Resume', 'Restart Song', 'Change difficulty', 'Exit to menu'];
+	
 	var curSelected:Int = 0;
 
 	var pauseMusic:FlxSound;
 
+	var focus:Int = 0;
+
 	public function new(x:Float, y:Float)
 	{
 		super();
+
+		if (PlayState.isStoryMode)
+			menuItems = ['Resume', 'Restart Song', 'Exit to menu'];
 
 		pauseMusic = new FlxSound().loadEmbedded(Paths.music('breakfast'), true, true);
 		pauseMusic.volume = 0;
@@ -104,9 +109,12 @@ class PauseSubState extends MusicBeatSubstate
 			switch (daSelected)
 			{
 				case "Resume":
+					PlayState.isPaused = false;
 					close();
 				case "Restart Song":
 					LoadingState.loadAndSwitchState(new PlayState());
+				case "Change difficulty":
+					changeItems(1);
 				case "Exit to menu":
 					if (PlayState.isStoryMode)
 						FlxG.switchState(new StoryMenuState());
@@ -117,6 +125,21 @@ class PauseSubState extends MusicBeatSubstate
 						// use it if we somehow broke lmao
 						FlxG.switchState(new MainMenuState());
 					}
+				case 'EASY':
+					PlayState.storyDifficulty = 0;
+					LoadingState.loadAndSwitchState(new PlayState());
+				case "NORMAL":
+					PlayState.storyDifficulty = 1;
+					LoadingState.loadAndSwitchState(new PlayState());
+				case "HARD":
+					PlayState.storyDifficulty = 2;
+					LoadingState.loadAndSwitchState(new PlayState());
+				case 'Back':
+					curSelected = 0;
+					changeItems(0);
+				default:
+					PlayState.storyDifficulty = 1;
+					LoadingState.loadAndSwitchState(new PlayState());
 			}
 		}
 
@@ -136,30 +159,65 @@ class PauseSubState extends MusicBeatSubstate
 		super.destroy();
 	}
 
-	function changeSelection(change:Int = 0):Void
-	{
-		curSelected += change;
+	function changeItems(focus:Int):Void
+		{
+			switch (focus)
+			{
+				case 0:
+					switch (PlayState.isStoryMode)
+					{
+						case false:
+							menuItems = ['Resume', 'Restart Song', 'Change difficulty', 'Exit to menu'];
+						case true:
+							// i understand but you can't do that
+							menuItems = ['Resume', 'Restart Song', 'Exit to menu'];
+					}
+				case 1:
+					menuItems = ['EASY', "NORMAL", "HARD", 'Back'];
+			}
+	
+			grpMenuShit.clear();
+	
+			curSelected = 0;
+			changeSelection();
+	
+			for (i in 0...menuItems.length)
+				{
+					var songText:Alphabet = new Alphabet(0, (70 * i) + 30, menuItems[i], true, false);
+					songText.isMenuItem = true;
+					songText.targetY = i;
+					grpMenuShit.add(songText);
+				}
+			// call it twice to fix white text??
+			changeSelection();
+		}
 
-		if (curSelected < 0)
+	function changeSelection(change:Int = 0):Void
+	{	
+			curSelected += change;
+
+			if (curSelected < 0)
 			curSelected = menuItems.length - 1;
-		if (curSelected >= menuItems.length)
+			if (curSelected >= menuItems.length)
 			curSelected = 0;
 
-		var bullShit:Int = 0;
+			var bullShit:Int = 0;
 
-		for (item in grpMenuShit.members)
-		{
-			item.targetY = bullShit - curSelected;
-			bullShit++;
-
-			item.alpha = 0.6;
-			// item.setGraphicSize(Std.int(item.width * 0.8));
-
-			if (item.targetY == 0)
+			for (item in grpMenuShit.members)
 			{
-				item.alpha = 1;
-				// item.setGraphicSize(Std.int(item.width));
+				item.targetY = bullShit - curSelected;
+				bullShit++;
+
+				item.alpha = 0.6;
+				// item.setGraphicSize(Std.int(item.width * 0.8));
+				item.selected = false;
+
+				if (item.targetY == 0)
+				{
+					item.alpha = 1;
+					// item.setGraphicSize(Std.int(item.width));
+					item.selected = true;
+				}
 			}
-		}
 	}
 }
