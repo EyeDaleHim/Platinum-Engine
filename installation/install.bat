884092a6879c38c89e1f62dad6b7870cc7486315
EyeDaleHim <eyedalehim306yt@gmail.com>
2021-09-10 00:16:52 +0800

    installation

---
+++ installation/install.bat
@@ -0,0 +1,19 @@
+haxelib install lime
+haxelib install openfl
+haxelib install flixel
+
+haxelib run lime setup flixel
+haxelib run lime setup
+
+haxelib install flixel-tools
+haxelib run flixel-tools setup
+
+haxelib git flixel-addons https://github.com/HaxeFlixel/flixel-addons
+
+haxelib git polymod https://github.com/larsiusprime/polymod.git
+haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
+
+haxelib install hscript
+haxelib install newgrounds
+
+exit
