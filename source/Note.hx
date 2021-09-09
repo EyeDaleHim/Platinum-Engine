48e3370d1cd410b150c5b29402a144d2bb93b072
EyeDaleHim <eyedalehim306yt@gmail.com>
2021-09-10 00:14:18 +0800

    use them in later versions

--- source/Note.hx
+++ source/Note.hx
@@ -29,16 +29,6 @@ class Note extends FlxSprite
 	public var noteScore:Float = 1;
 
 	public static var swagWidth:Float = 160 * 0.7;
-	// false if to disable them
-	public static var useNoteSkins:Bool = false;
-	public static var PURP_NOTE_CUSTOM_COLOR:FlxColor;
-	public static var GREEN_NOTE_CUSTOM_COLOR:FlxColor;
-	public static var RED_NOTE_CUSTOM_COLOR:FlxColor;
-	public static var BLUE_NOTE_CUSTOM_COLOR:FlxColor;
-	public static var PURP_NOTE:Int = 0;
-	public static var GREEN_NOTE:Int = 2;
-	public static var BLUE_NOTE:Int = 1;
-	public static var RED_NOTE:Int = 3;
 
 	public function new(strumTime:Float, noteData:Int, ?prevNote:Note, ?sustainNote:Bool = false)
 	{
